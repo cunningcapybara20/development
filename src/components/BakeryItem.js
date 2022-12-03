5af873ffd29b837cd3de2652f7c4c31e23e6bdc1
cunningcapybara20 <115045437+cunningcapybara20@users.noreply.github.com>
2022-12-03 00:01:38 -0500

    Create BakeryItem.js

---
+++ src/components/BakeryItem.js
@@ -0,0 +1,17 @@
+import './BakeryItem.css';
+export default function BakeryItem(props){
+    return(
+        <div className="item">
+        <img className="images" src={props.image} alt={props.name}/>
+        <h2>{props.name}</h2>
+        <p>{props.desc}</p>
+        <p>{props.price}</p>
+        <button onClick={() => {
+            props.addToCart(props.name, props.price, props.id)
+        }}>Add to Cart</button>
+        <button onClick={() => {
+            props.remove(props.id, props.price)
+        }}>Remove From Cart</button>
+        </div>
+    )
+}
